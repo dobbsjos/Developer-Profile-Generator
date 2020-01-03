3ed0260bf9ea1ab668556ea9bc3d5ccc43e0acf8
Joshua Dobbs <dobbsjos@gmail.com>
2020-01-03 09:40:55 -0500

    Axios call added but not working

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,21 @@
-const inquirer = require("inquirer")
-const axios = require("axios")
-const electron = require("electron")
-const generateHtml = require("./generateHTML")
+const inquirer = require("inquirer");
+const axios = require("axios");
+const electron = require("electron");
+const generateHtml = require("./generateHTML");
+const fs = require("fs");
+
+inquirer
+    .prompt({
+        message: "Enter your GitHub username",
+        name: "username"
+    });
+const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+axios.get(queryUrl)
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    });
